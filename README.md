ffab16d1f1b73407f90e713a21a8ae0f16557d93
David Jungst <djungst@gmail.com>
2018-10-22 15:30:56 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pull wiki content from Canvas
+
+1. Add canvas API KEY 
+1. Add course id
+1. ruby index.rb
+1. files will appear in folder under course id
+1. images will be broken
---
+++ index.rb
@@ -0,0 +1,52 @@
+require 'pry'
+require 'httparty'
+require 'fileutils'
+require 'dotenv'
+Dotenv.load
+
+BASE_URL = 'https://canvas.devpointlabs.com/api/v1'
+TOKEN = ENV['TOKEN']
+COURSE = ENV['COURSE']
+
+auth = {"Authorization" => "Bearer #{TOKEN}"}
+HTTParty::Basement.default_options.update(verify: false)
+url = "#{BASE_URL}/courses/#{COURSE}/pages?per_page=100"
+
+data = HTTParty.get(url, headers: auth )
+page = 1
+link = data.headers['link'].split("<https://canvas.devpointlabs.com").last
+md = link.match(/page=\d+/)
+pages = md[0].match(/\d+/)[0].match(/\d+/)[0].to_i
+ids = data.map { |d| d['html_url'].split("#{COURSE}/pages/").last }
+while page <= pages + 1
+  page = page + 1
+  url = "#{BASE_URL}/courses/#{COURSE}/pages?per_page=100&page=#{page}"
+  data = HTTParty.get(url, headers: auth )
+  ids = ids + data.map { |d| d['html_url'].split("#{COURSE}/pages/").last }
+end
+
+content = []
+
+ids.each do |id|
+  url = "#{BASE_URL}/courses/#{COURSE}/pages/#{id}"
+  data = HTTParty.get(url, headers: auth )
+  content << data
+end
+
+FileUtils.mkdir_p(COURSE.to_s) unless Dir.exists?(COURSE.to_s)
+
+total = 0
+content.each_with_index do |c, i|
+  begin
+    title = c['html_url'].split("#{COURSE}/pages/").last
+    puts "#{i}: #{title}"
+    File.open(File.join(Dir.pwd, "/#{COURSE}", "#{title}.html"), "w+") do |f|
+      f << c['body']
+    end
+    total += 1
+  rescue Exception => e
+    puts "ERROR: #{e}"
+  end
+end
+
+puts "#{total} files written to #{COURSE}/"
